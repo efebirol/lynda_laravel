b8a8cc1472f464caf4532fcf00bf5fb237327dd0
Birol <b.efe@compuccino.com>
2018-06-26 17:11:02 +0200

    Lynda Laravel Code with changes on routes.php

---
+++ .history/routes/web_20180626165709.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return '<h1>-- web.php - Hi, willkommen auf der Startseite</h1>';
+    // return view('welcome');
+});
+
+//Info: Array wird in eine JSON konvertiert von Laravel
+Route::get('/about', function () {
+    // return '<h1>-- web.php - Mit neuer URL Route bzw. Segment "about"</h1>';
+    $response_arr = [];
+    $response_arr['author'] = "testauthor";
+    $response_arr['version'] = "0.0.1";
+
+});
