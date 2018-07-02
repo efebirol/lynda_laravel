90ec60d6b163d3c9f7869f28af81043ecb6c7f0a
Birol <b.efe@compuccino.com>
2018-07-02 12:35:10 +0200

    Using existing Facades

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return '<h1>-- web.php - Hi, willkommen auf der Startseite</h1>';
@@ -31,4 +31,21 @@ Route::get('/home', function () {
     $data['version'] = "0.0.2";
     //übergebe hier 2 Parameter. 2te Parameter kann nun in der View verwendet werden über {{}}.
     return view('welcome', $data);
+});
+
+//Facade - DB Funktionen
+Route::get('/facades/db', function () {
+    return DB::select('select * from users');
+});
+
+//Facade - Verschlüsselung
+Route::get('/facades/encrypt', function () {
+    return Crypt::encrypt('123456789');
+});
+
+//Facade - Entschlüsselung
+Route::get('/facades/decrypt', function () {
+    //soll nun wieder entschlüsseln
+    return Crypt::decrypt('eyJpdiI6IjlRb3l4M2VqVFM0K1RPMDVzeFwvUEFRPT0iLCJ2YWx1ZSI6InpiOVBEenliaHJ3UUkwSDgrQnlOUzViZVhRUGRBdW5iS1MrZmg1Qys5eG89IiwibWFjIjoiMzZiZGZkMjgxYzY1ZmMxYWQwZjI1MGNmOTg5YWIwYjA1OTU3ZDVlOWE3MjI5YWFmNzJmNGU3NTQ3MmI0OGZhNyJ9
+    ');
 });
